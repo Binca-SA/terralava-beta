4c37c35cdfd0b5a0e6a46f01bc33e216a6eeb576
Binca-SA <binnas.shinaasi.asanova@gmail.com>
2022-03-23 14:16:26 +0100

    finishing slider-home
    - slider home-mobile finished, Missing changes in desktop version
    - FIXED MENU position issue in desktop
    -NEXT TASKS: continue with layout of the home section and start programming the operation of the filter with js

--- php/slider.php
+++ php/slider.php
@@ -6,11 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>slider</title>
     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="./css/main.css">
 
 </head>
 <body>
 <section class="contenedor-swiper">
-        <h1 class="titulo">terralava</h1>
+        <h1 class="title-jewlery">Terralava</h1>
         <div class="swiper mySwiper">
             <div class="swiper-wrapper">
 
@@ -21,7 +22,7 @@
                 <div class="swiper-slide">Slide 5</div>
             </div>
         </div>
-
+        <h2 class="subtitle-jewlery">Jewels</h2>
 
         <div class="swiper-button-prev"></div>
         <div class="swiper-button-next"></div>
